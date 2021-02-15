8943e9edaf3280ab99c0100f7bcbb9926f87a8a1
najuus <78907290+najuus@users.noreply.github.com>
2021-02-15 20:25:07 +0530

    student

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>my page</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<body>
+<table class="table">
+<tr>
+	<td>NAME</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>ADMISSION NO.</td>
+	<td><input class="form-control" type="text"</td>
+</tr>
+<tr>
+	<td>DEPARTMENT</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>COLLEGE NAME</td>
+	<td><input class="form-control  type="text"></td>
+</tr>
+<tr>
+	<td>PARENT NAME</td>
+	<td><input class="form-control" ype="text"></td>
+</tr>
+<tr>
+	<td>EMAIL ID</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>MOBILE NO.</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>USERNAME</td>
+	<td><input class="form-control" type="text"></td>
+</tr>
+<tr>
+	<td>PASSWORD</td>
+	<td><input class="form-control" type="password"></td>
+</tr>
+<tr>
+<td>CONFIRM PASSWORD</td>
+<td><input class="form-control" type="password"></td>
+</tr>
+<tr>
+	<td><button class="btn btn-danger"><b>REGISTER</b></button></td>
+</tr>
+</table>
+</body>
+</html>
